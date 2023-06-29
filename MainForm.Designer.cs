790dabaf5bee680c9d204c0182ad1b52dca4dae0
Hyguys <40918340+Hyguys@users.noreply.github.com>
2023-06-29 01:51:10 -0300

    Building - criei caixas de diálogo para conexão com Portas COM

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,24 +28,320 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.configuraçõesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.coletaDeDadosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.iniciarColetaDeDadosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.finalizarColetaDeDadosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.conexãoComArduinoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.conectarAoArduinoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.desconectarDoArduinoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sobreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ajudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sobreOProjetoPCAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sobreOMóduloToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sobreODesenvolvedorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manualDeOperaçãoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.treinamentoDoSupervisórioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tomadaDeDadosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.configuraçõesDeControleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formsPlot1 = new ScottPlot.FormsPlot();
+            this.configuraçõesDosGráficosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formsPlot2 = new ScottPlot.FormsPlot();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.White;
+            this.menuStrip1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem,
+            this.configuraçõesToolStripMenuItem,
+            this.ajudaToolStripMenuItem,
+            this.sobreToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(857, 29);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            this.arquivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.conexãoComArduinoToolStripMenuItem,
+            this.coletaDeDadosToolStripMenuItem});
+            this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(59, 25);
+            this.arquivoToolStripMenuItem.Text = "Geral";
+            // 
+            // configuraçõesToolStripMenuItem
+            // 
+            this.configuraçõesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tomadaDeDadosToolStripMenuItem,
+            this.configuraçõesDeControleToolStripMenuItem,
+            this.configuraçõesDosGráficosToolStripMenuItem});
+            this.configuraçõesToolStripMenuItem.Name = "configuraçõesToolStripMenuItem";
+            this.configuraçõesToolStripMenuItem.Size = new System.Drawing.Size(122, 25);
+            this.configuraçõesToolStripMenuItem.Text = "Configurações";
+            // 
+            // coletaDeDadosToolStripMenuItem
+            // 
+            this.coletaDeDadosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.iniciarColetaDeDadosToolStripMenuItem,
+            this.finalizarColetaDeDadosToolStripMenuItem});
+            this.coletaDeDadosToolStripMenuItem.Name = "coletaDeDadosToolStripMenuItem";
+            this.coletaDeDadosToolStripMenuItem.Size = new System.Drawing.Size(234, 26);
+            this.coletaDeDadosToolStripMenuItem.Text = "Coleta de Dados";
+            // 
+            // iniciarColetaDeDadosToolStripMenuItem
+            // 
+            this.iniciarColetaDeDadosToolStripMenuItem.Name = "iniciarColetaDeDadosToolStripMenuItem";
+            this.iniciarColetaDeDadosToolStripMenuItem.Size = new System.Drawing.Size(255, 26);
+            this.iniciarColetaDeDadosToolStripMenuItem.Text = "Iniciar Coleta de Dados";
+            // 
+            // finalizarColetaDeDadosToolStripMenuItem
+            // 
+            this.finalizarColetaDeDadosToolStripMenuItem.Name = "finalizarColetaDeDadosToolStripMenuItem";
+            this.finalizarColetaDeDadosToolStripMenuItem.Size = new System.Drawing.Size(255, 26);
+            this.finalizarColetaDeDadosToolStripMenuItem.Text = "Finalizar Coleta de Dados";
+            // 
+            // conexãoComArduinoToolStripMenuItem
+            // 
+            this.conexãoComArduinoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.conectarAoArduinoToolStripMenuItem,
+            this.desconectarDoArduinoToolStripMenuItem});
+            this.conexãoComArduinoToolStripMenuItem.Name = "conexãoComArduinoToolStripMenuItem";
+            this.conexãoComArduinoToolStripMenuItem.Size = new System.Drawing.Size(234, 26);
+            this.conexãoComArduinoToolStripMenuItem.Text = "Conexão com Arduino";
+            // 
+            // conectarAoArduinoToolStripMenuItem
+            // 
+            this.conectarAoArduinoToolStripMenuItem.Name = "conectarAoArduinoToolStripMenuItem";
+            this.conectarAoArduinoToolStripMenuItem.Size = new System.Drawing.Size(247, 26);
+            this.conectarAoArduinoToolStripMenuItem.Text = "Conectar ao Arduino";
+            // 
+            // desconectarDoArduinoToolStripMenuItem
+            // 
+            this.desconectarDoArduinoToolStripMenuItem.Name = "desconectarDoArduinoToolStripMenuItem";
+            this.desconectarDoArduinoToolStripMenuItem.Size = new System.Drawing.Size(247, 26);
+            this.desconectarDoArduinoToolStripMenuItem.Text = "Desconectar do Arduino";
+            // 
+            // sobreToolStripMenuItem
+            // 
+            this.sobreToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sobreOProjetoPCAToolStripMenuItem,
+            this.sobreOMóduloToolStripMenuItem,
+            this.sobreODesenvolvedorToolStripMenuItem});
+            this.sobreToolStripMenuItem.Name = "sobreToolStripMenuItem";
+            this.sobreToolStripMenuItem.Size = new System.Drawing.Size(63, 25);
+            this.sobreToolStripMenuItem.Text = "Sobre";
+            // 
+            // ajudaToolStripMenuItem
+            // 
+            this.ajudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.manualDeOperaçãoToolStripMenuItem,
+            this.treinamentoDoSupervisórioToolStripMenuItem});
+            this.ajudaToolStripMenuItem.Name = "ajudaToolStripMenuItem";
+            this.ajudaToolStripMenuItem.Size = new System.Drawing.Size(62, 25);
+            this.ajudaToolStripMenuItem.Text = "Ajuda";
+            // 
+            // sobreOProjetoPCAToolStripMenuItem
+            // 
+            this.sobreOProjetoPCAToolStripMenuItem.Name = "sobreOProjetoPCAToolStripMenuItem";
+            this.sobreOProjetoPCAToolStripMenuItem.Size = new System.Drawing.Size(242, 26);
+            this.sobreOProjetoPCAToolStripMenuItem.Text = "Sobre o Projeto PCA";
+            // 
+            // sobreOMóduloToolStripMenuItem
+            // 
+            this.sobreOMóduloToolStripMenuItem.Name = "sobreOMóduloToolStripMenuItem";
+            this.sobreOMóduloToolStripMenuItem.Size = new System.Drawing.Size(242, 26);
+            this.sobreOMóduloToolStripMenuItem.Text = "Sobre o Módulo";
+            // 
+            // sobreODesenvolvedorToolStripMenuItem
+            // 
+            this.sobreODesenvolvedorToolStripMenuItem.Name = "sobreODesenvolvedorToolStripMenuItem";
+            this.sobreODesenvolvedorToolStripMenuItem.Size = new System.Drawing.Size(242, 26);
+            this.sobreODesenvolvedorToolStripMenuItem.Text = "Sobre o Desenvolvedor";
+            // 
+            // manualDeOperaçãoToolStripMenuItem
+            // 
+            this.manualDeOperaçãoToolStripMenuItem.Name = "manualDeOperaçãoToolStripMenuItem";
+            this.manualDeOperaçãoToolStripMenuItem.Size = new System.Drawing.Size(281, 26);
+            this.manualDeOperaçãoToolStripMenuItem.Text = "Manual de Operação";
+            // 
+            // treinamentoDoSupervisórioToolStripMenuItem
+            // 
+            this.treinamentoDoSupervisórioToolStripMenuItem.Name = "treinamentoDoSupervisórioToolStripMenuItem";
+            this.treinamentoDoSupervisórioToolStripMenuItem.Size = new System.Drawing.Size(281, 26);
+            this.treinamentoDoSupervisórioToolStripMenuItem.Text = "Treinamento do Supervisório";
+            // 
+            // tomadaDeDadosToolStripMenuItem
+            // 
+            this.tomadaDeDadosToolStripMenuItem.Name = "tomadaDeDadosToolStripMenuItem";
+            this.tomadaDeDadosToolStripMenuItem.Size = new System.Drawing.Size(309, 26);
+            this.tomadaDeDadosToolStripMenuItem.Text = "Tomada de Dados e Amostragem";
+            this.tomadaDeDadosToolStripMenuItem.Click += new System.EventHandler(this.tomadaDeDadosToolStripMenuItem_Click);
+            // 
+            // configuraçõesDeControleToolStripMenuItem
+            // 
+            this.configuraçõesDeControleToolStripMenuItem.Name = "configuraçõesDeControleToolStripMenuItem";
+            this.configuraçõesDeControleToolStripMenuItem.Size = new System.Drawing.Size(265, 26);
+            this.configuraçõesDeControleToolStripMenuItem.Text = "Configurações de Controle";
+            // 
+            // formsPlot1
+            // 
+            this.formsPlot1.AutoSize = true;
+            this.formsPlot1.Location = new System.Drawing.Point(415, 34);
+            this.formsPlot1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.formsPlot1.Name = "formsPlot1";
+            this.formsPlot1.Size = new System.Drawing.Size(427, 221);
+            this.formsPlot1.TabIndex = 1;
+            // 
+            // configuraçõesDosGráficosToolStripMenuItem
+            // 
+            this.configuraçõesDosGráficosToolStripMenuItem.Name = "configuraçõesDosGráficosToolStripMenuItem";
+            this.configuraçõesDosGráficosToolStripMenuItem.Size = new System.Drawing.Size(309, 26);
+            this.configuraçõesDosGráficosToolStripMenuItem.Text = "Configurações dos Gráficos";
+            // 
+            // formsPlot2
+            // 
+            this.formsPlot2.Location = new System.Drawing.Point(415, 251);
+            this.formsPlot2.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
+            this.formsPlot2.Name = "formsPlot2";
+            this.formsPlot2.Size = new System.Drawing.Size(427, 240);
+            this.formsPlot2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(12, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(161, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Conexão com Arduino:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(179, 34);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(126, 22);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "Não conectado!";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Supervisório_PCA_2._0.Properties.Resources.logoextensa;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 432);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(252, 65);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 32);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Buscar Portas COM";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(179, 62);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 29);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Font = new System.Drawing.Font("Segoe UI", 8F);
+            this.textBox3.Location = new System.Drawing.Point(651, 482);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(194, 15);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "Desenvolvido por Leandro Favaretto";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(957, 574);
+            this.ClientSize = new System.Drawing.Size(857, 509);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.formsPlot2);
+            this.Controls.Add(this.formsPlot1);
+            this.Controls.Add(this.menuStrip1);
             this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
             this.Name = "MainForm";
-            this.Text = "Main";
+            this.Text = "SUPERVISÓRIO 2.0 PROJETO DE CONTROLE COM ARDUINO by Hyguys";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem configuraçõesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem conexãoComArduinoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem conectarAoArduinoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem desconectarDoArduinoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem coletaDeDadosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem iniciarColetaDeDadosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem finalizarColetaDeDadosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ajudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sobreToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sobreOProjetoPCAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sobreOMóduloToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sobreODesenvolvedorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tomadaDeDadosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem configuraçõesDeControleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem manualDeOperaçãoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem treinamentoDoSupervisórioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem configuraçõesDosGráficosToolStripMenuItem;
+        private ScottPlot.FormsPlot formsPlot1;
+        private ScottPlot.FormsPlot formsPlot2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -7,14 +7,105 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
 
 namespace Supervisório_PCA_2._0
 {
     public partial class MainForm : Form
     {
+        private SerialPort serialPort;
+
         public MainForm()
         {
             InitializeComponent();
+            serialPort = new SerialPort(); // Adicione essa linha para inicializar o objeto serialPort
+        }
+
+        private void tomadaDeDadosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SearchSerialPorts();
+        }
+
+        private void SearchSerialPorts()
+        {
+            // Limpar a lista de portas COM existente
+            comboBox1.Items.Clear();
+
+            // Obter todas as portas COM disponíveis no sistema
+            string[] portNames = SerialPort.GetPortNames();
+
+            // Verificar se há portas COM disponíveis
+            if (portNames.Length > 0)
+            {
+                if (portNames.Length == 1)
+                {
+                    MessageBox.Show(portNames.Length + " porta COM foi encontrada!", "Porta COM encontrada!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show(portNames.Length + " portas COM foram encontradas!", "Portas COM encontradas!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+                // Adicionar as portas COM à lista
+                comboBox1.Items.AddRange(portNames);
+                comboBox1.SelectedIndex = 0; // Definir a primeira porta como selecionada
+            }
+            else
+            {
+                DialogResult result = MessageBox.Show("Nenhuma porta COM foi encontrada.", "Erro!", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                if (result == DialogResult.Retry)
+                {
+                    SearchSerialPorts(); // Chama o método de pesquisa novamente
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedPort = comboBox1.SelectedItem.ToString();
+
+            // Exiba uma mensagem de confirmação
+            DialogResult result = MessageBox.Show("Deseja conectar-se à porta COM " + selectedPort + " selecionada?", "Confirmação de conexão", MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                // Conecte-se à porta COM selecionada
+                try
+                {
+                    serialPort.PortName = selectedPort;
+                    serialPort.Open();
+                    MessageBox.Show("Conexão à porta " + selectedPort + " estabelecida com sucesso!","Conexão realizada com sucesso!",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro ao conectar-se à porta COM: " + ex.Message,"Erro na conexão!",MessageBoxButtons.RetryCancel,MessageBoxIcon.Warning);
+                }
+            }
+            else
+            {
+                // Cancelar a conexão
+                MessageBox.Show("Conexão cancelada pelo usuário.","Conexão cancelada!", MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
         }
     }
 }
